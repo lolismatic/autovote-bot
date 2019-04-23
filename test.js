e5cea5707c443cc5c70d22f6d4773ed1b475fae1
Dan Patiu <dan.patiu@pitechnologies.ro>
2019-04-23 11:25:08 +0300

    feat(): add bot logic

---
+++ test.js
@@ -0,0 +1,32 @@
+// Import requirement packages
+require('chromedriver');
+const assert = require('assert');
+const { Builder, Key, By, until } = require('selenium-webdriver');
+
+describe('Login & vote Dalaran Wow', function () {
+    let driver;
+
+    before(async function() {
+        driver = await new Builder().forBrowser('chrome').build();
+    });
+    // Next, we will write steps for our test.
+    // For the element ID, you can find it by open the browser inspect feature.
+    it('Login to Dalaran Wow', async function() {
+        // Load the page
+        await driver.get('https://www.dalaran-wow.com/account-log/');
+        // Find the search box by id
+        await driver.findElement(By.id('accountName')).sendKeys('{{USERNAME}}');
+        // Enter keywords and click enter
+        await driver.findElement(By.id('password')).sendKeys('{{PASSWORD}}', Key.RETURN);
+        // Wait for the results box by id
+        await driver.wait(until.elementLocated(By.className('service-welcome')), 20000);
+        // We will get the title value and test it
+        await driver.get('https://www.dalaran-wow.com/account/vote/1');
+        await driver.get('https://www.dalaran-wow.com/account/vote/2');
+        await driver.get('https://www.dalaran-wow.com/account/vote/3');
+        await driver.get('https://www.dalaran-wow.com/account/vote/4');
+    });
+
+    // close the browser after running tests
+    after(() => driver && driver.quit());
+})
